0912f8ca852ccbccb25085354cff12b93f5dbdf1
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2020-01-19 18:00:09 +0100

    changed double well to H2+

--- quantumdraw/server/levels.py
+++ quantumdraw/server/levels.py
@@ -5,20 +5,24 @@ import torch
 # with a Torch tensor similar to an array of x-values.
 
 def level1(pos):
+    '''harmonic oscillator.'''
     return pos**2
 
 def level2(pos):
+    '''morse oscillator.'''
     if isinstance(pos, torch.Tensor):
         return 0.25*(torch.exp(-2.*(pos+2)) - 2.*torch.exp(-(pos+2))).view(-1, 1) 
     else:
         return 0.25*(np.exp(-2.*(pos+2)) - 2.*np.exp(-(pos+2))) 
         
 def level3(pos):
+    '''Double well'''
     h = 0.75
     c = 0.05
     return -0.25*pos**2*h + 0.5 * c*pos**4
 
 def level4(pos):
+    ''' infinite well.'''
     if isinstance(pos, torch.Tensor):
         v = 0.1*torch.ones_like(pos)
         v[pos<-2.5] = 5.
@@ -28,8 +32,16 @@ def level4(pos):
     else:
         return .1 if np.abs(pos) < 2.5 else 5.
 
+
+def level5(pos):
+    k=0.5*1E-1
+    if isinstance(pos,torch.Tensor):
+        return -k/torch.abs(pos-2.5) + -k/torch.abs(pos+2.5)
+    else:
+        return -k/np.abs(pos-2.5) + -k/np.abs(pos+2.5)
+
 potentials = [
     level1,
     level2,
-    level3
+    level5
 ]
